f7568beebcd97c5793f71ee1198220debd047c2f
Felippe <felippepg.goncalves@outlook.com.br>
2023-10-19 10:24:51 -0300

    Ajustando imagens sobre os projetos

--- src/feature/Projects/index.tsx
+++ src/feature/Projects/index.tsx
@@ -20,27 +20,58 @@ const Projects = () => {
   }, []);
 
   return (
-    <section className="mt-96 element">
+    <section className="mt-80 laptop:mt-96 element">
       <Title>{t('title')}</Title>
       <h3 className="text-orange-thema text-center mt-10">{t('subtitle')}</h3>
 
-      <div className="w-full mx-auto flex flex-col items-center text-orange-thema">
-        {/* Conteúdo da primeira coluna */}
-        <div className="w-full flex flex-col laptop:flex-row laptop:justify-between mb-4 p-4">
-          <div className="w-full laptop:w-80 desktop:w-96 h-52 bg-blue-300 p-4">
-            1
+      <div className="w-full mx-auto flex flex-col items-center text-orange-thema mt-5">
+        <div className="w-full flex flex-col laptop:flex-row laptop:justify-between mb-4 p-0">
+          <div className="w-full laptop:w-80 desktop:w-96 p-1 h-52 mb-5">
+            <h1 className="text-orange-thema text-center pb-5">Repo Radar</h1>
+
+            <img
+              className="w-full h-full object-cover"
+              width="auto"
+              src="/img/repo-radar.svg"
+              alt="Imagem do projeto Repo Radar"
+            />
           </div>
-          <div className="w-full laptop:w-80 desktop:w-96 h-52 bg-blue-300 p-4">
-            2
+          <div className="w-full laptop:w-80 desktop:w-96 p-1 h-52 mt-5">
+            <h1 className="text-orange-thema pb-5 text-center">
+              Sorteador de Amigo Secreto
+            </h1>
+
+            <img
+              className="w-full h-full object-cover"
+              width="auto"
+              src="/img/sorteador.svg"
+              alt="Imagem do projeto Sorteador"
+            />
           </div>
         </div>
 
-        <div className="w-full flex flex-col laptop:flex-row laptop:justify-between mb-4 p-4">
-          <div className="w-full laptop:w-80 desktop:w-96 h-52 bg-blue-300 p-4">
-            3
+        <div className="w-full flex flex-col laptop:flex-row laptop:justify-between mb-4 p-0">
+          <div className="w-full laptop:w-80 desktop:w-96 p-1 h-52 mt-5">
+            <h1 className="text-orange-thema pb-5 text-center">
+              Voll Med - API
+            </h1>
+
+            <img
+              className="w-full h-full object-cover"
+              width="auto"
+              src="/img/vol-med.svg"
+              alt="Imagem do projeto Voll Med API"
+            />
           </div>
-          <div className="w-full laptop:w-80 desktop:w-96 h-52 bg-blue-300 p-4">
-            4
+          <div className="w-full laptop:w-80 desktop:w-96 p-1 h-52 mt-10">
+            <h1 className="text-orange-thema pb-5 text-center">Piresfood</h1>
+
+            <img
+              className="w-full h-full object-cover"
+              width="auto"
+              src="/img/piresfood.svg"
+              alt="Imagem do projeto Piresfood"
+            />
           </div>
         </div>
       </div>
